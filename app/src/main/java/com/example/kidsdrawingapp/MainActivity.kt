032400efe70c331e4a601f2c3536c818514cc7f8
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-11 11:07:57 +0500

    Main Activity: Added -> a share feature to share the image via-Email - Whatsapp etc

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -216,7 +217,11 @@ class MainActivity : AppCompatActivity() {
                     runOnUiThread {
                         cancelProgressDialog()
                         if(result.isNotEmpty()){
-                            Toast.makeText(this@MainActivity, "File save successfully", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File save successfully :$result",
+                                Toast.LENGTH_SHORT).show()
+                            shareImage(result)
                         }else {
                             Toast.makeText(
                                 this@MainActivity,
@@ -243,6 +248,17 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog != null){
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result: String){
+        MediaScannerConnection.scanFile(this, arrayOf(result),null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM,uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
         }
     }
 }
